396a6ba6e43a4905822589a97e4e7fc54c25913a
Valérie <br3adina7or@gmail.com>
2023-06-30 09:19:52 +0100

    Added day 1 part 2 to the ocaml solution

--- 2022/day1/ocaml/bin/main.ml
+++ 2022/day1/ocaml/bin/main.ml
@@ -4,16 +4,23 @@ let () =
   if Array.length Sys.argv == 1 then begin
     print_endline "Expected input file as command-line argument";
     flush stdout;
-    exit 1
+    exit 1;
   end;
 
   let path = Array.get Sys.argv 1 in
   let ic = open_in path in
     try
-      print_int (Reader.read ic 0 0);
+      let top3 = Reader.read ic 0 { a = 0; b = 0; c = 0 } in
+      print_endline "Top 3:";
+      print_int top3.a;
       print_newline ();
-      flush stdout;
-      close_in ic;
+      print_int top3.b;
+      print_newline ();
+      print_int top3.c;
+      print_newline ();
+      print_endline "\nSum of top 3:";
+      print_int (top3.a + top3.b + top3.c);
+      print_newline ();
       exit 0
     with e ->
       close_in_noerr ic;
--- 2022/day1/ocaml/lib/reader.ml
+++ 2022/day1/ocaml/lib/reader.ml
@@ -1,11 +1,22 @@
-let sum l = List.fold_left (+) 0 l;;
+(* Could be generic? *)
+type top3 = {
+  a : int;
+  b : int;
+  c : int
+}
 
-let rec read ic cur max =
+let push_top3 item cur =
+  if cur.a < item then      { a = item;  b = cur.a; c = cur.b }
+  else if cur.b < item then { a = cur.a; b = item;  c = cur.b }
+  else if cur.c < item then { a = cur.a; b = cur.b; c = item }
+  else cur
+
+let rec read ic cur cur_top3 =
   match input_line ic with
-  | "" -> read ic 0 (Int.max max cur)
+  | "" -> read ic 0 (push_top3 cur cur_top3)
   | line -> begin
     let v = int_of_string line in
-    read ic (v + cur) max
+    read ic (v + cur) cur_top3
   end
-  | exception End_of_file -> Int.max max cur
+  | exception End_of_file -> push_top3 cur cur_top3 
 

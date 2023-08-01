30fbe00f6380d917a175de0e88b3786a0ce0de04
br3adina7or <br3adina7or@gmail.com>
2023-08-01 12:34:19 +0100

    added some unit tests, but no additional functionality

--- 2022/day5/ocaml/lib/common.ml
+++ 2022/day5/ocaml/lib/common.ml
@@ -1,6 +1,11 @@
 let (>>=) = Result.bind
 let (>>=|>) x y = x >>= fun z -> z |> y
 
+(* So I don't have to `open Core` and break everything ^^ *)
+let compare_char = Core.compare_char
+let char_of_sexp = Core.char_of_sexp
+let sexp_of_char = Core.sexp_of_char
+
 type crane_operation = {
     n: int;
     from_idx: int;
@@ -10,6 +15,7 @@ type crane_operation = {
 type parse_crates_error =
     | InvalidCrateConfiguration
     | InvalidValue of char
+[@@deriving compare, sexp]
 
 let get_opt arr idx =
     if idx < Array.length arr then Some (Array.get arr idx)
@@ -25,37 +31,41 @@ let print_stacks stacks =
     in
     Array.iter print_stack stacks
 
+(** Parses the initial configuration of crates.
+
+Expects an input such as: `\["[A] [B]"; "[C] [D]"; " 1  2 "]`*)
 let parse_crates lines =
-    let rec parse_line stacks line n num_stacks =
-        if String.length line == 0 then
-            Ok stacks
-        else if (String.length line + 1) == (num_stacks - n) * 4 then
+    let rec parse_line stacks line n =
+        if String.length line == 0 then Ok stacks
+        else begin
             (* if stmt bc it throws if oob *)
             let tl = if String.length line > 4 then Str.string_after line 4 else "" in
             match String.get line 1 with
             | ('A'..'Z' as ch) ->
                 stacks.(n) <- ch :: stacks.(n);
-                parse_line stacks tl (n + 1) num_stacks
-            | ' ' -> parse_line stacks tl (n + 1) num_stacks
+                parse_line stacks tl (n + 1)
+            | ' ' -> parse_line stacks tl (n + 1)
             | ch -> Error (InvalidValue ch)
-        else
-            Error InvalidCrateConfiguration
+        end
     in
 
-    let rec handle_line rem_lines num_stacks stacks =
+    let rec handle_line rem_lines stacks =
         match rem_lines with
-        | hd :: tl -> parse_line stacks hd 0 num_stacks >>=|> handle_line tl num_stacks
+        | hd :: tl ->
+            if ((String.length hd + 1) mod 4) == 0 then
+                parse_line stacks hd 0 >>=|> handle_line tl
+            else Error InvalidCrateConfiguration
         | [] -> Ok stacks
     in
 
     match List.rev lines with
     | labels :: crates ->
-            let num_stacks = (String.length labels + 1) / 4 in
-            handle_line crates num_stacks (Array.make num_stacks [])
+            let num_stacks = ((String.length labels + 1) / 4) in
+            handle_line crates (Array.make num_stacks [])
     | [] -> Error InvalidCrateConfiguration
 
 let solve _crane ic =
-    (* this just takes the first tines of the input *)
+    (** This just takes the first lines of the input, up until the first empty line. *)
     let rec input_starting_positions ic acc =
         let temp = input_line ic in
         if String.length temp == 0 then acc
@@ -65,15 +75,9 @@ let solve _crane ic =
     let stacks = input_starting_positions ic []
     |> parse_crates in
 
-    (* debug print *)
-    (* probably a better way to do this *)
     let _ = match stacks with
     | Ok stacks -> print_stacks stacks
-    | Error InvalidCrateConfiguration -> print_endline "InvalidCrateConfiguration"
-    | Error (InvalidValue ch) ->
-        print_string "InvalidValue: ";
-        print_char ch;
-        print_newline ()
+    | Error err -> sexp_of_parse_crates_error err |> Core.Sexp.to_string |> print_endline
     in
 
     ""
---
+++ 2022/day5/ocaml/test/parse_initial_config.ml
@@ -0,0 +1,37 @@
+open Core
+open Aoc22d5
+
+let%test_unit "basic" =
+    let res = Common.parse_crates [
+        "[A] [B]";
+        "[C] [D]";
+        " 1   2 "
+    ] in
+    let expected = Ok (Array.of_list [['A'; 'C']; ['B'; 'D']]) in
+    [%test_eq: (char list array, Common.parse_crates_error) Result.t] res expected
+
+let%test_unit "real" =
+    let res = Common.parse_crates [
+        "                [M]     [W] [M]    ";
+        "            [L] [Q] [S] [C] [R]    ";
+        "            [Q] [F] [F] [T] [N] [S]";
+        "    [N]     [V] [V] [H] [L] [J] [D]";
+        "    [D] [D] [W] [P] [G] [R] [D] [F]";
+        "[T] [T] [M] [G] [G] [Q] [N] [W] [L]";
+        "[Z] [H] [F] [J] [D] [Z] [S] [H] [Q]";
+        "[B] [V] [B] [T] [W] [V] [Z] [Z] [M]";
+        " 1   2   3   4   5   6   7   8   9 "
+    ] in
+    let expected = Ok (Array.of_list [
+        ['T'; 'Z'; 'B'];
+        ['N'; 'D'; 'T'; 'H'; 'V'];
+        ['D'; 'M'; 'F'; 'B'];
+        ['L'; 'Q'; 'V'; 'W'; 'G'; 'J'; 'T'];
+        ['M'; 'Q'; 'F'; 'V'; 'P'; 'G'; 'D'; 'W'];
+        ['S'; 'F'; 'H'; 'G'; 'Q'; 'Z'; 'V'];
+        ['W'; 'C'; 'T'; 'L'; 'R'; 'N'; 'S'; 'Z'];
+        ['M'; 'R'; 'N'; 'J'; 'D'; 'W'; 'H'; 'Z'];
+        ['S'; 'D'; 'F'; 'L'; 'Q'; 'M']
+    ]) in
+    [%test_eq: (char list array, Common.parse_crates_error) Result.t] res expected
+
